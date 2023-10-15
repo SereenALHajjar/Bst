9dadcf6403c89fb0ce3afe055cdc7c9e27e5bdea
Sereen <sereen.alhajjar@gmail.com>
2023-10-15 11:26:05 +0300

    Co-authored-by: Abdullah Alattar <abdullah.mohammad.alattar@gmail.com>

--- src/Bst.cpp
+++ src/Bst.cpp
@@ -1,44 +1,92 @@
 #include <Bst.hpp>
 #include <iostream>
+#include<vector>
 using namespace std;
 
-Bst::Bst() { root = NULL; }
-node *Bst::insert(int value) {
-  if (root == NULL) {
+// Bst::Bst() { root = NULL; }
+void Bst::insert(int value)
+{
+  if (root == NULL)
+  {
     root = new node;
     root->data = value;
     root->left = NULL;
     root->right = NULL;
-    return root;
-  } else
-    return insert(root, value);
+  }
+  else
+    insert(root, value);
 }
-node *Bst::insert(node *current, int value) {
+void Bst::insert(node *current, int value)
+{
 
-  if (value < current->data) {
+  if (value < current->data)
+  {
     if (current->left != NULL)
       insert(current->left, value);
-    else {
+    else
+    {
       node *newNode = new node;
       newNode->left = newNode->right = NULL;
       newNode->data = value;
       current->left = newNode;
     }
   }
-  if (value > current->data) {
+  if (value > current->data)
+  {
     if (current->right != NULL)
       insert(current->right, value);
-    else {
+    else
+    {
       node *newNode = new node;
       newNode->left = newNode->right = NULL;
       newNode->data = value;
       current->right = newNode;
     }
   }
-  // TODO(Sereen): Why does insert return a value?
-  return current;
 }
-node *Bst::find(node *current, int value) {
+node * findMinimum(node * current)
+{
+  if(current == NULL) 
+    return NULL ;
+  if(current->left != NULL) 
+    return findMinimum(current->left) ;
+  else return current ;
+}
+void Bst ::remove(int value)
+{
+  remove(root, value);
+}
+node * Bst ::remove(node *current, int value)
+{
+  if (current == NULL)
+    return NULL ;
+  if (value > current->data)
+    current->right = remove(current->right, value);
+  else if (value < current->data)
+    current->left = remove(current->left, value);
+  else
+  {
+    if(current->left == current->right && current->left == NULL)
+      delete current ;
+    else if(current->right != NULL && current->left !=NULL)
+    {
+      node * x = findMinimum(current->right) ;
+      current->data = x->data ;
+      current->right = remove(current->right , current->data) ;
+    }
+    else 
+    {
+      node * temp = current ;
+      if(current->left == NULL)
+        current = current->right ;
+      else current = current->left ;
+      delete temp ;
+    }
+  }
+  return current ;
+}
+node *Bst::find(node *current, int value)
+{
   if (current->data == value)
     return current;
   node *ans = NULL;
@@ -48,34 +96,43 @@ node *Bst::find(node *current, int value) {
     ans = find(current->right, value);
   return ans;
 }
-node *Bst::find(int value) { return find(root, value); }
-void Bst::inorder(node *current) {
+node *Bst::find(int value)
+{
+  return find(root, value);
+}
+void Bst::inorder(node *current , vector<int> & v)
+{
   if (current == NULL)
     return;
-  inorder(current->left);
-  cout << current->data << " ";
-  inorder(current->right);
+  inorder(current->left , v);
+  v.push_back(current->data) ;
+  inorder(current->right , v);
 }
-void Bst::postorder(node *current) {
+void Bst::postorder(node *current , vector<int> & v)
+{
   if (current == NULL)
     return;
-  postorder(current->left);
-  postorder(current->right);
-  cout << current->data << " ";
+  postorder(current->left , v);
+  postorder(current->right , v);
+  v.push_back(current->data) ;
 }
-void Bst::preorder(node *current) {
+void Bst::preorder(node *current, vector<int>& v)
+{
   if (current == NULL)
     return;
-  cout << current->data << " ";
-  preorder(current->left);
-  preorder(current->right);
+  v.push_back(current->data);
+  preorder(current->left , v);
+  preorder(current->right , v);
 }
-void Bst::display(string type) {
+void Bst::display(string type)
+{
+  vector<int>v ; 
   if (type == "preorder")
-    preorder(root);
+    preorder(root, v);
   if (type == "postorder")
-    postorder(root);
+    postorder(root , v);
   if (type == "inorder")
-    inorder(root);
+    inorder(root , v);
+  for(int i=0 ; i<v.size() ; i++) cout<<v[i]<<" " ;
+  cout<<endl ;
 }
-
--- src/Bst.hpp
+++ src/Bst.hpp
@@ -1,5 +1,6 @@
+#pragma once
 #include <string>
-
+#include <vector>
 struct node {
   node *left;
   node *right;
@@ -9,20 +10,28 @@ struct node {
 class Bst {
 
 public:
-  Bst();
-  node *insert(int value);
+  Bst()
+  {
+    root = NULL;
+  }
 
-  node *insert(node *current, int value);
+  void insert(int value);
+
+  void insert(node *current, int value);
+
+  void remove(int value) ;
+
+  node * remove(node * current , int value) ;
 
   node *find(node *current, int value);
 
   node *find(int value);
 
-  void inorder(node *current);
+  void inorder(node *current , std::vector<int> & );
 
-  void postorder(node *current);
+  void postorder(node *current , std::vector<int> &);
 
-  void preorder(node *current);
+  void preorder(node *current, std::vector<int>& );
 
   void display(std::string type);
 
